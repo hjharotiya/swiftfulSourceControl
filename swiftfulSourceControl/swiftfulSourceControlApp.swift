9fbcd37a24c6ad29f66ef4ca1f737ff266d77285
harshit jharotiya <jharotiyaharshit@gmail.com>
2025-03-20 13:54:23 +0530

    Initial Commit

---
+++ swiftfulSourceControl/swiftfulSourceControlApp.swift
@@ -0,0 +1,17 @@
+//
+//  swiftfulSourceControlApp.swift
+//  swiftfulSourceControl
+//
+//  Created by Harshit Jharotiya on 20/03/25.
+//
+
+import SwiftUI
+
+@main
+struct swiftfulSourceControlApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
