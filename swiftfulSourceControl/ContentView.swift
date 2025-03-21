c1528771c6813dd791d620db815bbdfba4934da9
harshit jharotiya <jharotiyaharshit@gmail.com>
2025-03-21 13:21:30 +0530

    add home view

--- swiftfulSourceControl/ContentView.swift
+++ swiftfulSourceControl/ContentView.swift
@@ -14,6 +14,13 @@ struct ContentView: View {
                 .font(.largeTitle)
                 .foregroundStyle(.tint)
           Text("i dont need it ")
+            
+            Button {
+                
+            } label: {
+                Text("hey there")
+            }
+
 
         }
         .padding()
---
+++ swiftfulSourceControl/HomeView.swift
@@ -0,0 +1,18 @@
+//
+//  HomeView.swift
+//  swiftfulSourceControl
+//
+//  Created by Harshit Jharotiya on 21/03/25.
+//
+
+import SwiftUI
+
+struct HomeView: View {
+    var body: some View {
+        Text("this is where i am learning github")
+    }
+}
+
+#Preview {
+    HomeView()
+}
